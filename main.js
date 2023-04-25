4b5bb84f8e5fb2dabb75b805232edc4403277369
LePangolin <91209500+LePangolin@users.noreply.github.com>
2023-04-25 09:50:34 +0200

    feat(vr): deplacement

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
-import { VRButton } from 'three/addons/webxr/VRButton.js';
+import { VRButton } from "three/addons/webxr/VRButton.js";
 
-import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory.js';
+import { XRControllerModelFactory } from "three/examples/jsm/webxr/XRControllerModelFactory.js";
 
 import Stats from "three/examples/jsm/libs/stats.module.js";
 
@@ -139,12 +139,10 @@ textureLoader.load(
   }
 );
 
-
 for (let i = 0; i < 10; i++) {
   fbxLoader.load(
     "./sources/oak 01.fbx",
     (fbx) => {
-
       let randX = Math.random() * 400 - 100;
       let randZ = Math.random() * 400 - 100;
       fbx.position.set(randX, -10, randZ);
@@ -192,7 +190,6 @@ for (let i = 0; i < 10; i++) {
   );
 }
 
-
 loadingManager.onLoad = () => {
   console.log("loaded");
   animatevr();
@@ -203,14 +200,12 @@ const renderer = new THREE.WebGLRenderer({
   antialias: false,
 });
 
-
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
 renderer.xr.enabled = true;
 renderer.xr.setReferenceSpaceType("local");
-document.body.appendChild( VRButton.createButton( renderer ) );
+document.body.appendChild(VRButton.createButton(renderer));
 let gripController1 = renderer.xr.getControllerGrip(0);
 
 const model1 = new XRControllerModelFactory();
@@ -233,30 +228,157 @@ gripController2.addEventListener("selectstart", onSelectStart);
 gripController2.addEventListener("selectend", onSelectEnd);
 scene.add(gripController2);
 
-
-
-function onSelectStart(event) {
-  scene.add(cube);
+function onSelectStart() {
+  this.userData.isSelecting = true;
 }
 
-function onSelectEnd(event) {
-  scene.remove(cube);
+function onSelectEnd() {
+  this.userData.isSelecting = false;
+
+  if (INTERSECTION) {
+    const offsetPosition = {
+      x: -INTERSECTION.x,
+      y: -INTERSECTION.y,
+      z: -INTERSECTION.z,
+      w: 1,
+    };
+    const offsetRotation = new THREE.Quaternion();
+    const transform = new XRRigidTransform(offsetPosition, offsetRotation);
+    const teleportSpaceOffset =
+      baseReferenceSpace.getOffsetReferenceSpace(transform);
+
+    renderer.xr.setReferenceSpace(teleportSpaceOffset);
+  }
 }
 
-function animatevr(){
-  let moving = false;
+controller1 = renderer.xr.getController(0);
+controller1.addEventListener("selectstart", onSelectStart);
+controller1.addEventListener("selectend", onSelectEnd);
+controller1.addEventListener("connected", function (event) {
+  this.add(buildController(event.data));
+});
+controller1.addEventListener("disconnected", function () {
+  this.remove(this.children[0]);
+});
+scene.add(controller1);
 
-  renderer.setAnimationLoop(() => {
-    groups.position.y = -10;
-    camera.position.y = 47;
-    renderer.render(scene, camera);
-  });
+controller2 = renderer.xr.getController(1);
+controller2.addEventListener("selectstart", onSelectStart);
+controller2.addEventListener("selectend", onSelectEnd);
+controller2.addEventListener("connected", function (event) {
+  this.add(buildController(event.data));
+});
+controller2.addEventListener("disconnected", function () {
+  this.remove(this.children[0]);
+});
+scene.add(controller2);
+
+// The XRControllerModelFactory will automatically fetch controller models
+// that match what the user is holding as closely as possible. The models
+// should be attached to the object returned from getControllerGrip in
+// order to match the orientation of the held device.
+
+const controllerModelFactory = new XRControllerModelFactory();
+
+controllerGrip1 = renderer.xr.getControllerGrip(0);
+controllerGrip1.add(
+  controllerModelFactory.createControllerModel(controllerGrip1)
+);
+scene.add(controllerGrip1);
+
+controllerGrip2 = renderer.xr.getControllerGrip(1);
+controllerGrip2.add(
+  controllerModelFactory.createControllerModel(controllerGrip2)
+);
+scene.add(controllerGrip2);
+
+function buildController(data) {
+  let geometry, material;
+
+  switch (data.targetRayMode) {
+    case "tracked-pointer":
+      geometry = new THREE.BufferGeometry();
+      geometry.setAttribute(
+        "position",
+        new THREE.Float32BufferAttribute([0, 0, 0, 0, 0, -1], 3)
+      );
+      geometry.setAttribute(
+        "color",
+        new THREE.Float32BufferAttribute([0.5, 0.5, 0.5, 0, 0, 0], 3)
+      );
+
+      material = new THREE.LineBasicMaterial({
+        vertexColors: true,
+        blending: THREE.AdditiveBlending,
+      });
+
+      return new THREE.Line(geometry, material);
+
+    case "gaze":
+      geometry = new THREE.RingGeometry(0.02, 0.04, 32).translate(0, 0, -1);
+      material = new THREE.MeshBasicMaterial({
+        opacity: 0.5,
+        transparent: true,
+      });
+      return new THREE.Mesh(geometry, material);
+  }
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 
+function animate() {
+  renderer.setAnimationLoop(render);
+}
 
+function render() {
+  INTERSECTION = undefined;
 
+  if (controller1.userData.isSelecting === true) {
+    tempMatrix.identity().extractRotation(controller1.matrixWorld);
 
+    raycaster.ray.origin.setFromMatrixPosition(controller1.matrixWorld);
+    raycaster.ray.direction.set(0, 0, -1).applyMatrix4(tempMatrix);
+
+    const intersects = raycaster.intersectObjects([floor]);
+
+    if (intersects.length > 0) {
+      INTERSECTION = intersects[0].point;
+    }
+  } else if (controller2.userData.isSelecting === true) {
+    tempMatrix.identity().extractRotation(controller2.matrixWorld);
+
+    raycaster.ray.origin.setFromMatrixPosition(controller2.matrixWorld);
+    raycaster.ray.direction.set(0, 0, -1).applyMatrix4(tempMatrix);
+
+    const intersects = raycaster.intersectObjects([floor]);
+
+    if (intersects.length > 0) {
+      INTERSECTION = intersects[0].point;
+    }
+  }
+
+  if (INTERSECTION) marker.position.copy(INTERSECTION);
+
+  marker.visible = INTERSECTION !== undefined;
+
+  renderer.render(scene, camera);
+}
+
+// function animatevr() {
+//   let moving = false;
+
+//   renderer.setAnimationLoop(() => {
+//     groups.position.y = -10;
+//     camera.position.y = 47;
+//     renderer.render(scene, camera);
+//   });
+// }
 
 window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -288,7 +410,6 @@ raycaster = new THREE.Raycaster(
   10
 );
 
-
 // function animate() {
 //   requestAnimationFrame(animate);
 //   const time = performance.now();
@@ -296,7 +417,6 @@ raycaster = new THREE.Raycaster(
 //   carHitbox = new THREE.Box3().setFromObject(groups);
 //   carHitbox = carHitbox.expandByScalar(2);
 
-  
 //   carHitboxHelper = new THREE.Box3Helper(carHitbox, 0xffff00);
 //   if (carHitboxHelper) {
 //     scene.add(carHitboxHelper);
@@ -405,7 +525,6 @@ raycaster = new THREE.Raycaster(
 //   renderer.render(scene, camera);
 // }
 
-
 document.addEventListener("keydown", function (event) {
   if (event.code === "ArrowUp" || event.code === "KeyW") {
     moveForward = true;
@@ -435,3 +554,6 @@ document.addEventListener("keyup", function (event) {
     moveRight = false;
   }
 });
+
+
+animate();
