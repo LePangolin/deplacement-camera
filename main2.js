845ec192a46212a7aabbefa8bc6136483159f4ac
LePangolin <91209500+LePangolin@users.noreply.github.com>
2023-04-26 10:44:56 +0200

    debug(vr): deplacement gerbotron

--- main2.js
+++ main2.js
@@ -266,17 +266,18 @@ function render() {
   }
 
   if (controller1.userData.isSelecting === true) {
-    tempMatrix.identity().extractRotation(controller1.matrixWorld);
+    // tempMatrix.identity().extractRotation(controller1.matrixWorld);
 
-    raycaster.ray.origin.setFromMatrixPosition(controller1.matrixWorld);
-    raycaster.ray.direction.set(0, 0, -1).applyMatrix4(tempMatrix);
+    // raycaster.ray.origin.setFromMatrixPosition(controller1.matrixWorld);
+    // raycaster.ray.direction.set(0, 0, -1).applyMatrix4(tempMatrix);
 
-    const intersects = raycaster.intersectObjects([floor]);
-    console.log(intersects);
+    // const intersects = raycaster.intersectObjects([floor]);
+    // console.log(intersects);
 
-    if (intersects.length > 0) {
-      INTERSECTION = intersects[0].point;
-    }
+    // if (intersects.length > 0) {
+    //   INTERSECTION = intersects[0].point;
+    // }
+    camera.position.x += 0.1;
   } else if (controller2.userData.isSelecting === true) {
     tempMatrix.identity().extractRotation(controller2.matrixWorld);
 
